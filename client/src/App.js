f0b303ab429a1e97c9a98ca8bb994478cafac3b1
Antowa-qq <anton-vinsss@mail.ru>
2021-11-18 04:26:23 +0300

    added fn initialdb

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { Loader } from './components';
-
 import { Dogs } from './pages';
+import { initialDB } from './api/initialDB';
 
 function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setLoading(false);
+    const fetchData = async () => {
+      await initialDB();
+      setLoading(false);
+    };
+    fetchData();
   }, []);
-  
+
   return (
     <>
       {loading && <Loader />}
